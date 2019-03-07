e945637f71b95af04963b1b57020151ab899a369
dmantadakis <dainstain.1879@gmail.com>
2019-03-07 01:58:26 +0100

    Small simplifications + Generators + Algorithm as class

--- rush_objects/rush_solver.py
+++ rush_objects/rush_solver.py
@@ -1,30 +1,25 @@
-3#######################################################
-#  Based on: https://github.com/aimacode/aima-python  #
-#######################################################
-# from utils import memoize, PriorityQueue
 from rush_objects.rushhour_objects import Board
 from collections import deque
 from abc import ABC, abstractmethod
-
+from functools import partial 
 
 class Problem(ABC):
     """
     The abstract class for a formal problem.
-    """
 
+    Arguments:
+    ----------
+    initial: state
+    goal   : state 
+    """
     def __init__(self, initial, goal=None):
-        """
-        The constructor specifies the initial state, and possibly a goal
-        state, if there is a unique goal.
-        """
         self.initial = initial
         self.goal = goal
 
     @abstractmethod
     def actions(self, state):
         """
-        Return the actions that can be executed in the given
-        state.
+        Return the actions that can be executed in the given state.
         """
         pass
 
@@ -52,20 +47,19 @@ class Problem(ABC):
     def path_cost(self, c, state1, action, state2):
         """
         Return the cost of a solution path that arrives at state2 from
-        state1 via action, assuming cost c to get up to state1.
-        If the problem
-        is such that the path doesn't matter, this function will only look at
-        state2.
-        If the path does matter, it will consider c and maybe state1
-        and action. The default method costs 1 for every step in the path.
+        state1 via action, assuming cost c to get up to state1. If the 
+        problem is such that the path doesn't matter, this function will 
+        only look at state2. If the path does matter, it will consider c 
+        and maybe state1 and action. 
+        
+        The default method costs 1 for every step in the path.
         """
         return c + 1
 
     def value(self, state):
         """
-        For optimization problems, each state has a value.
-        Hill-climbing
-        and related algorithms try to maximize this value.
+        For optimization problems, each state has a value; e.g. algorithms
+        such as Hill-climbing try to maximize this value
         """
         raise NotImplementedError
 # ______________________________________________________________________________
@@ -96,7 +90,7 @@ class Node(ABC):
         self.action = action
         self.path_cost = path_cost
         self.depth = 0
-        self.parent_list = []
+        self.parent_list = list() 
         if parent:
             self.depth = parent.depth + 1
             self.parent_list = parent.parent_list + [self.state]
@@ -113,38 +107,35 @@ class Node(ABC):
     def expand(self, problem):
         """
         List the nodes reachable in one step from this node.
-        """
-        return [self.child_node(problem, action)
-                for action in problem.actions(self.state)]
+        """       
+        return (self.child_node(problem,action) 
+                for action in problem.actions(self.state))
+
 
     def child_node(self, problem, action):
-        next_state = problem.result(self.state, action)
-        next_node = Node(next_state, self, action,
-                         problem.path_cost(self.path_cost, self.state,
-                                           action, next_state))
-        return next_node
+        """
+        Given the action it constructs the next (child) node 
+        """
+        node = problem.result(self.state, action)
+        cost = problem.path_cost(self.path_cost, self.state, action, node)
+        return Node(node, self, action, cost) 
 
     def solution(self):
         """
         Return the sequence of actions to go from the root to this node.
         """
-        return [node.action for node in self.path()[1:]]
+        return map(lambda x: x.action, self.path()[1:]) 
 
     def path(self):
         """
         Return a list of nodes forming the path from the root to this node.
         """
-        node = self
-        path_back = [node.state]
-        for i in range(self.depth):
-            node = node.parent
-            path_back.append(node.state)
-        return list(reversed(path_back))
-
-    # We want  a queue of nodes in breadth_first_graph_search or
-    # astar_search to have no duplicated states, so we treat nodes
-    # with the same state as equal. [Problem: this may not be what you
-    # want in other contexts.]
+        last_node = self 
+        traceback = [last_node.state]
+        for index in range(self.depth):
+            last_node = last_node.parent
+            traceback.append(last_node.state)
+        return list(reversed(traceback)) 
 
     def __eq__(self, other):
         return isinstance(other, Node) and self.state == other.state
@@ -152,38 +143,80 @@ class Node(ABC):
     def __hash__(self):
         return hash(self.state)
 
-def breadth_first_graph_search(problem):
-    """[Figure 3.11]
-    Note that this function can be implemented in a
-    single line as below:
-    return graph_search(problem, FIFOQueue())
+class BreadthFirstGraphSeach(object):
     """
-    node = Node(problem.initial)
-    if problem.goal_test(node.state):
-        return node
-    frontier = deque([node])
-    explored = set()
-    i = 0
+    Alternative way to define a function
+    """
+    def __init__(self,problem):
+        if not isinstance(problem,Problem):
+            raise ValueError("Algorithm expects a instance of a Problem")
+        self.problem = problem 
+        self.root = problem.initial
+        self.frontier = deque([Node(self.root)])
+        self.explored = set() 
+        self.iterations = 0
+
+        # Check if the problem that has been provided is trivial
+        if self.problem.goal_test(self.root):
+            print("Initial state of the problem is already a solution") 
+
+    def __call__(self,max_depth=500,printouts=100):
+        while self.frontier:
+            node = self.frontier.popleft()
+            self.explored.add(node.state)
+
+            for child in node.expand(self.problem):
+                if child.state not in self.explored and child not in self.frontier:
+                    if self.problem.goal_test(child.state):
+                        print("Solution found in %s steps"%self.iterations)
+                        return child
+                    self.frontier.append(child)
+
+            self.iterations += 1
+            if self.iterations%printouts == 0:
+                print("- Checked already {0} nodes".format(self.iterations))
+            if node.depth == max_depth:
+                print("Could not find solution within the 'depth' limit")
+                return False
+
+        print("Solution could not be found within the limits imposed")
+        self.iterations = 0
+        self.frontier = deque([Node(self.root)])
+        self.explored = set() 
+        return False
+
+
+def breadth_first_graph_search(problem,max_depth=500,printouts=100):
+    """
+    TODO: WRITE DOCUMENTATION FOR THIS ALGORITHM 
+    """
+    root = Node(problem.initial)
+    if problem.goal_test(root.state): return root
+
+    frontier = deque([root])
+    explored = set() 
+    steps = 0
 
     while frontier:
         node = frontier.popleft()
         explored.add(node.state)
+
         for child in node.expand(problem):
             if child.state not in explored and child not in frontier:
                 if problem.goal_test(child.state):
+                    print("Solution found in %s steps"%steps)
                     return child
                 frontier.append(child)
-        i += 1
-        if i % 1000 == 0:
-            print('Checked already {} nodes'.format(i))
-        if node.depth == 500:
-            print('Could not find solution within 100 steps, sorry!')
+
+        steps += 1
+        if steps%printouts == 0:
+            print("- Checked alread {0} nodes".format(steps))
+        if node.depth == max_depth:
+            print("Could not find solution within the 'depth' limit")
             return False
-    print('Solution could not be found within the limits imposed')
+
+    print("Solution could not be found within the limits imposed")
     return False
-
-
-
 
 class RushGame(Problem):
     """
@@ -201,8 +234,6 @@ class RushGame(Problem):
 
     """
     def __init__(self, board, **kwargs):
-
-        print(type(board))
         if isinstance(board, Board):
             self.initial = Board.from_state(board.get_state())
         elif isinstance(board, tuple):
@@ -221,9 +252,8 @@ class RushGame(Problem):
             else:
                 targetcol = self.initial.view.shape[0] - self.initial[self.target_car].length
                 self.target = (self.initial.exitrow, targetcol)
-        self.board = self.initial
-        # The problem instance has to be initialized with the state
-        Problem.__init__(self, self.board.get_state(), None)
+
+        Problem.__init__(self, self.initial.get_state(), None)
 
     def actions(self, state):
         """
@@ -241,9 +271,7 @@ class RushGame(Problem):
             please note that in this case there is a perfect equivalence between
             action and state that can be reached with it.
         """
-        self.board = self.board.from_state(state)
-        actions = self.board.connected_states
-        return actions
+        return Board.from_state(state).connected_states
 
     def result(self, state, action):
         """
@@ -257,7 +285,7 @@ class RushGame(Problem):
         Returns:
         action, the state to be reached
         """
-        assert state != action
+        #assert state != action
         return action
 
     def h(self, node):
@@ -270,8 +298,8 @@ class RushGame(Problem):
         """
         Check if target car is in proper position
         """
-        self.board = self.board.from_state(state)
-        return self.board.cars[self.target_car].position == self.target
+        board = Board.from_state(state)
+        return board.cars[self.target_car].position == self.target
 
     def solve(self, astar=False):
         """
@@ -282,5 +310,22 @@ class RushGame(Problem):
             return astar_search(self)
         else:
             return breadth_first_graph_search(self)
+
+# Conceptually and notationally its more correct to 
+# - Define the board
+# - Defind the game (which is a Problem) by providing the board and the rules
+# - Feed this game into a Solver 
+
+
+
+
+
+
+
+
+
+
+
+
 
 
